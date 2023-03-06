49df6db08539f1eed2ac10937d6d08030e8563de
CK Mirafuente <mcmirafuente@gmail.com>
2023-03-06 20:09:56 +0800

    initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+module.exports = {
+  webpack: (config, { dev }) => {
+    // Use Sass for CSS files
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: "sass-loader",
+          options: {
+            sassOptions: {
+              includePaths: ["./node_modules"],
+            },
+          },
+        },
+      ],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
